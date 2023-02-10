1d085df72ab8ab51978ca79861d8428153ed8c08
Alexander Kosyakov <110721351+gOweRkrd@users.noreply.github.com>
2023-02-10 09:33:41 +0700

    Add files via upload

---
+++ СhoiceОftraining/Sources/Module/MainScreen/Controller/MainController.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+final class MainController: UIViewController {
+    
+    // MARK: - Properties
+    
+    //    private let paymentView = PaymentView()
+    
+    // MARK: - Lifecycle
+    
+//    override func loadView() {
+//        super.loadView()
+//        self.view = paymentView
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+    }
+    
+}
